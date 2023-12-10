4e89391f75e304471dc9ee657512706abd3fd5ac
Victor Umesiobi <Vicksemmanuel58@gmail.com>
2023-12-10 19:54:46 +0000

    fix: server fixed

--- api/myapp.py
+++ api/myapp.py
@@ -6,50 +6,56 @@ from PIL import Image
 import torch.nn as nn
 import io
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-# Load your model
-DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-NUM_CLASSES = 3
-model = models.densenet121(pretrained=False)
-num_ftrs = model.classifier.in_features
-model.classifier = nn.Linear(num_ftrs, NUM_CLASSES)
-model.load_state_dict(torch.load("model.pth"))
-model.to(DEVICE)
-model.eval()
+    # Load your model
+    DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+    NUM_CLASSES = 3
+    model = models.densenet121(pretrained=False)
+    num_ftrs = model.classifier.in_features
+    model.classifier = nn.Linear(num_ftrs, NUM_CLASSES)
+    model.load_state_dict(torch.load("model.pth"))
+    model.to(DEVICE)
+    model.eval()
 
-@app.route('/')
-def welcome():
-    return "Welcome to the API!"
+    @app.route('/')
+    def welcome():
+        return "Welcome to the API!"
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    if request.method == 'POST':
-        # Convert the Image from request to a PIL Image
-        image = Image.open(io.BytesIO(request.files['image'].read()))
+    @app.route('/predict', methods=['POST'])
+    def predict():
+        if request.method == 'POST':
+            # Convert the Image from request to a PIL Image
+            image = Image.open(io.BytesIO(request.files['image'].read()))
 
-        # Preprocess the image and prepare it for your model
-        preprocess =  transforms.Compose([
-            transforms.Resize((224, 224)),
-            transforms.Grayscale(num_output_channels=3), 
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        ])
-        input_tensor = preprocess(image)
-        input_batch = input_tensor.unsqueeze(0)
+            # Preprocess the image and prepare it for your model
+            preprocess =  transforms.Compose([
+                transforms.Resize((224, 224)),
+                transforms.Grayscale(num_output_channels=3), 
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ])
+            input_tensor = preprocess(image)
+            input_batch = input_tensor.unsqueeze(0)
 
-        # Run inference
-        with torch.no_grad():
-            output = model(input_batch)  # Unsqueeze to add a batch dimension (batch size of 1)
-            _, predicted_class = torch.max(output, 1)
+            # Run inference
+            with torch.no_grad():
+                output = model(input_batch)  # Unsqueeze to add a batch dimension (batch size of 1)
+                _, predicted_class = torch.max(output, 1)
 
-        # Return the response as JSON
+            # Return the response as JSON
 
-        class_labels = ["covid", "normal", "pneumonia"]
-        predicted_label = class_labels[predicted_class.item()]
+            class_labels = ["covid", "normal", "pneumonia"]
+            predicted_label = class_labels[predicted_class.item()]
 
-        return jsonify(result=predicted_label)
+            return jsonify(result=predicted_label)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    return app
+
+
+
+# if __name__ == '__main__':
+#     app = create_app()
+#     app.run(debug=True)
