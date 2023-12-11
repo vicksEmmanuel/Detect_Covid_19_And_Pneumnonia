8034d26191cafb67dfb3224bdaf4abff97aad91c
Victor Umesiobi <Vicksemmanuel58@gmail.com>
2023-12-11 11:34:48 +0000

    fix: bring back start up

--- api/run.py
+++ api/run.py
@@ -3,6 +3,6 @@ from myapp import create_app
 app = create_app()
 
 
-# if __name__ == '__main__':
-#     app = create_app()
-#     app.run(host="0.0.0.0", port=8000)
+if __name__ == '__main__':
+    app = create_app()
+    app.run(host="0.0.0.0", port=8000)
