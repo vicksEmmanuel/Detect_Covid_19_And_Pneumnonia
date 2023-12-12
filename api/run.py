a8db059fad5a3aeca5a2c73ca4e6be1c53101c31
Victor Umesiobi <Vicksemmanuel58@gmail.com>
2023-12-12 18:22:49 +0000

    fix: minor change

--- api/run.py
+++ api/run.py
@@ -5,7 +5,8 @@ app = create_app()
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    # app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run()
 
 
 # Path: api/myapp/__init__.py
